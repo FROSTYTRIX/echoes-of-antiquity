1eb465e7e290547d41accf48abb4c08fdcd55a34
frostytrix <frostytrix13@gmail.com>
2026-03-05 22:42:47 +0100

    updated dragon bow

--- src/main/java/net/frostytrix/echoesofantiquity/item/custom/DragonBowItem.java
+++ src/main/java/net/frostytrix/echoesofantiquity/item/custom/DragonBowItem.java
@@ -1,10 +1,19 @@
 package net.frostytrix.echoesofantiquity.item.custom;
 
+import net.minecraft.entity.FlyingItemEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.BowItem;
+import org.jetbrains.annotations.Nullable;
 
 public class DragonBowItem extends BowItem {
     public DragonBowItem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    protected void shoot(LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, @Nullable LivingEntity target) {
+        projectile.setVelocity(shooter, shooter.getPitch(), shooter.getYaw() + yaw, 0.0F, speed * 2, divergence);
     }
 
     @Override
