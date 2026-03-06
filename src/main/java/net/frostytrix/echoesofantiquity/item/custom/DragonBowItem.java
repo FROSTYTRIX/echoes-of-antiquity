0026fc007247da7a83a2c925e13f6fbfe81455a2
FROSTYTRIX <frostytrix13@gmail.com>
2026-03-06 19:20:10 +0100

    Updated the Dragon Bow to be more precise !

--- src/main/java/net/frostytrix/echoesofantiquity/item/custom/DragonBowItem.java
+++ src/main/java/net/frostytrix/echoesofantiquity/item/custom/DragonBowItem.java
@@ -1,9 +1,18 @@
 package net.frostytrix.echoesofantiquity.item.custom;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.BowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DragonBowItem extends BowItem {
     public DragonBowItem(Settings settings) {
@@ -13,6 +22,35 @@ public class DragonBowItem extends BowItem {
     @Override
     protected void shoot(LivingEntity shooter, ProjectileEntity projectile, int index, float speed, float divergence, float yaw, @Nullable LivingEntity target) {
         projectile.setVelocity(shooter, shooter.getPitch(), shooter.getYaw() + yaw, 0.0F, speed * 2, divergence);
+    }
+
+    @Override
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity playerEntity) {
+            ItemStack itemStack = playerEntity.getProjectileType(stack);
+            if (!itemStack.isEmpty()) {
+                int i = this.getMaxUseTime(stack, user) - remainingUseTicks;
+                float f = getPullProgress(i);
+                if (!(f < 0.1)) {
+                    List<ItemStack> list = load(stack, itemStack, playerEntity);
+                    if (world instanceof ServerWorld serverWorld && !list.isEmpty()) {
+                        this.shootAll(serverWorld, playerEntity, playerEntity.getActiveHand(), stack, list, f * 3.0F, 0.4F, f == 1.0F, null);
+                    }
+
+                    world.playSound(
+                            null,
+                            playerEntity.getX(),
+                            playerEntity.getY(),
+                            playerEntity.getZ(),
+                            SoundEvents.ENTITY_ARROW_SHOOT,
+                            SoundCategory.PLAYERS,
+                            1.0F,
+                            1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F
+                    );
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                }
+            }
+        }
     }
 
     @Override
