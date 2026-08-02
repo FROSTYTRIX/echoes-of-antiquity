fb28dfdb49a1c3bff7987db9f12e6f8fb271d9d7
FROSTYTRIX <frostytrix13@gmail.com>
2026-08-02 22:27:39 +0200

    Cleaned up the loot table modifiers, the mansion had two identical blocks

--- src/main/java/net/frostytrix/echoesofantiquity/util/ModLootTableModifiers.java
+++ src/main/java/net/frostytrix/echoesofantiquity/util/ModLootTableModifiers.java
@@ -2,6 +2,8 @@ package net.frostytrix.echoesofantiquity.util;
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.frostytrix.echoesofantiquity.item.ModItems;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.ItemConvertible;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
@@ -9,68 +11,39 @@ import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
-import net.minecraft.util.Identifier;
 
 public class ModLootTableModifiers {
-    private static final Identifier CAVE_SPIDER_ID =
-            Identifier.of("minecraft", "entities/cave_spider");
 
     public static void modifyLootTables() {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
-            if (CAVE_SPIDER_ID.equals(key.getValue())){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.005F))
-                        .with(ItemEntry.builder(ModItems.CLIMBING_SPIDER_LEG))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 1.0F)).build());
-            tableBuilder.pool(poolBuilder.build());
+            if (EntityType.CAVE_SPIDER.getLootTableId().equals(key)) {
+                tableBuilder.pool(chanceDrop(ModItems.CLIMBING_SPIDER_LEG, 0.005F, 1, 1));
             }
 
-            if (LootTables.PILLAGER_OUTPOST_CHEST.equals(key)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.1F))
-                        .with(ItemEntry.builder(ModItems.SOUL_FRAGMENT))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 2.0F)).build());
-                tableBuilder.pool(poolBuilder.build());
+            if (LootTables.PILLAGER_OUTPOST_CHEST.equals(key)) {
+                tableBuilder.pool(chanceDrop(ModItems.SOUL_FRAGMENT, 0.1F, 1, 2));
             }
 
-            if (LootTables.WOODLAND_MANSION_CHEST.equals(key)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.14F))
-                        .with(ItemEntry.builder(ModItems.SOUL_FRAGMENT))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 2.0F)).build());
-                tableBuilder.pool(poolBuilder.build());
+            if (LootTables.WOODLAND_MANSION_CHEST.equals(key)) {
+                tableBuilder.pool(chanceDrop(ModItems.SOUL_FRAGMENT, 0.14F, 1, 2));
+                tableBuilder.pool(chanceDrop(ModItems.SOUL_SIPHON, 0.20F, 1, 1));
             }
 
-            if (LootTables.WOODLAND_MANSION_CHEST.equals(key)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.20F))
-                        .with(ItemEntry.builder(ModItems.SOUL_SIPHON))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 1.0F)).build());
-                tableBuilder.pool(poolBuilder.build());
+            if (LootTables.END_CITY_TREASURE_CHEST.equals(key)) {
+                tableBuilder.pool(chanceDrop(ModItems.END_STEEL_UPGRADE, 0.35F, 1, 1));
             }
 
-            if (LootTables.END_CITY_TREASURE_CHEST.equals(key)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.35F))
-                        .with(ItemEntry.builder(ModItems.END_STEEL_UPGRADE))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 1.0F)).build());
-                tableBuilder.pool(poolBuilder.build());
-            }
-
-            if (LootTables.STRONGHOLD_CORRIDOR_CHEST.equals(key)){
-                LootPool.Builder poolBuilder = LootPool.builder()
-                        .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(0.10F))
-                        .with(ItemEntry.builder(ModItems.END_STEEL_UPGRADE))
-                        .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0F, 1.0F)).build());
-                tableBuilder.pool(poolBuilder.build());
+            if (LootTables.STRONGHOLD_CORRIDOR_CHEST.equals(key)) {
+                tableBuilder.pool(chanceDrop(ModItems.END_STEEL_UPGRADE, 0.10F, 1, 1));
             }
         });
     }
 
+    private static LootPool.Builder chanceDrop(ItemConvertible item, float chance, float min, float max) {
+        return LootPool.builder()
+                .rolls(ConstantLootNumberProvider.create(1))
+                .conditionally(RandomChanceLootCondition.builder(chance))
+                .with(ItemEntry.builder(item))
+                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(min, max)).build());
+    }
 }
